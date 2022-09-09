903e2b9c33ecd0fb26ee78d4f93061a7910e75dd
Renata Sisilio <112419444+RenataSisilio@users.noreply.github.com>
2022-09-08 22:01:14 -0300

    Update dartfolio.dart

--- bin/dartfolio.dart
+++ bin/dartfolio.dart
@@ -2,4 +2,6 @@ void main() {
   print('Bem vindo(a) a Turma 001');
   print('-----------------------------');
   print('No  Nome                Idade');
+  print('-----------------------------');
+  print('1   Renata               27');
 }
