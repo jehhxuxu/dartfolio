9c7abb8ab81073576d6e094cbef8428aa7bebb01
cmalassise <caiocmalassise@gmail.com>
2022-09-08 21:56:29 -0300

    usuario_caio_adicionado

--- bin/dartfolio.dart
+++ bin/dartfolio.dart
@@ -1,5 +1,6 @@
 void main(List<String> arguments) {
   print('Bem vindo(a) a Turma 001');
   print('-----------------------------');
-  print('No: 05 - Nome: Caio Malassise - Idade: 34');
+  print('No  Nome                Idade');
+  print('05  Caio Malassise      34');
 }
